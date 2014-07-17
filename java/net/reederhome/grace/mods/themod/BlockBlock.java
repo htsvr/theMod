bf8eca487ff2ff6a556b5daf90a3d6df6eb690ca
Grace Reeder <brrgrra@gmail.com>
2014-07-17 15:14:42 -0600

    added block

---
+++ java/net/reederhome/grace/mods/themod/BlockBlock.java
@@ -0,0 +1,23 @@
+package net.reederhome.grace.mods.themod;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.util.IIcon;
+
+public class BlockBlock extends Block {
+
+	Block block;
+	public BlockBlock() {
+		super(Material.rock);
+		do {
+			Object[] keys = Block.blockRegistry.getKeys().toArray();
+			block = (Block)Block.blockRegistry.getObject(keys[new Random().nextInt(keys.length)]);
+		} while(block==this);
+		System.out.println(block.getUnlocalizedName());
+	}
+	public IIcon getIcon(int s, int m) {
+		return block.getIcon(s, m);
+	}
+}
--- java/net/reederhome/grace/mods/themod/TheMod.java
+++ java/net/reederhome/grace/mods/themod/TheMod.java
@@ -1,16 +1,20 @@
 package net.reederhome.grace.mods.themod;
 
+import net.minecraft.block.Block;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid=TheMod.MODID,name=TheMod.NAME)
 public class TheMod {
 	public static final String MODID="themod";
 	public static final String NAME="The Mod";
 	
+	public static Block blockBlock=new BlockBlock();
+	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent ev) {
-		
+		GameRegistry.registerBlock(blockBlock, "blockBlock");
 	}
 }
