6c6570667ee4c00d79d86326ff6da062dcf08c02
Colin Reeder <vpzomtrrfrt@gmail.com>
2015-05-23 17:00:03 -0600

    Remove my unneccesary comments

--- java/net/reederhome/grace/mods/themod/TheMod.java
+++ java/net/reederhome/grace/mods/themod/TheMod.java
@@ -25,9 +25,7 @@ public class TheMod {
 		GameRegistry.registerBlock(blockBlock, "blockBlock");
 		GameRegistry.registerItem(itemItem, "itemItem");
 		
-		//GameRegistry.addRecipe(new ItemStack(itemItem),"byb","oso","ryr",'b',new ItemStack(Items.dye, 1, 4),'y',new ItemStack(Items.dye, 1, 11),'o',new ItemStack(Items.dye, 1, 14),'s',Items.stick,'r',Items.redstone);
 		GameRegistry.addRecipe(new ShapedOreRecipe(itemItem, "byb", "oso", "ryr", 'b', "dyeBlue", 'y', "dyeYellow", 'o', "dyeOrange", 's', "stickWood", 'r', "dustRedstone"));
-		//GameRegistry.addRecipe(new ItemStack(blockBlock),"scs","wIw","rir",'s',Blocks.stone,'c',Blocks.cobblestone,'w',Blocks.planks,'I',itemItem,'r',Items.redstone,'i',Items.iron_ingot);
 		GameRegistry.addRecipe(new ShapedOreRecipe(blockBlock, "scs", "wIw", "rir", 's', "stone", 'c', "cobblestone", 'w', "plankWood", 'I', itemItem, 'r', "dustRedstone", 'i', "ingotIron"));
 	}
 }
